022bc7b5ac3091f358bd5db2fb951574a376ad3c
GianaTk7 <tkgenia1234@gmail.com>
2024-11-27 10:55:00 +0200

    added login popup

--- script.js
+++ script.js
@@ -76,3 +76,21 @@ closeButtons.forEach(button => {
         document.body.classList.remove("popup-active");
     });
 });
+
+
+//styling accordion
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
